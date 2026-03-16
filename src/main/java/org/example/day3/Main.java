f56f766b2a2e15ffc5da2a54d591f88760456344
vladanidze <vladyslav.zhernovyi@gmail.com>
2026-03-16 10:24:04 +0100

    third Day

---
+++ src/main/java/org/example/day3/Main.java
@@ -0,0 +1,22 @@
+package org.example.day3;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        TestCase tc1 = new TestCase();
+        ArrayList<TestCase> testCases = new ArrayList<>();
+        tc1.setName("case 1");
+        tc1.setPassed(true);
+        testCases.add(tc1);
+
+        TestCase tc2 = new TestCase("case 2", true);
+        testCases.add(tc2);
+
+        testCases.add(new TestCase("case 3", false));
+
+        for (TestCase item: testCases){
+            item.printStatus();
+        }
+    }
+}
